3b165b0b38644197d5f9e7e48982a79126aa75c7
Simão Menezes <simaomenezesneto@gmail.com>
2017-04-24 10:46:01 -0300

    Criando e configurando provider para serviço rest.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { RestapiServices } from '../providers/restapi-services';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -34,7 +36,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler}, RestapiService
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { RestapiService } from '../../providers/restapi-services';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
+users: any;
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public restapiService: RestapiService) {
+  	this.getUsers();
   }
 
+}
+
+
+getUsers(){
+	this.restapiService.getUsers().then(data => {
+		this.users = data;
+	});
 }
